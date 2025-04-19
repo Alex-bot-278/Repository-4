c849e55582229b2bc87748249c3c256f34fed962
Alex-bot-278 <sasha.rybakov1997@mail.ru>
2025-04-19 19:41:43 +0500

    refactor: разделил код и тесты на отдельные файлы

--- precode.go
+++ precode.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"net/url"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -48,43 +43,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-// Тест 1: Корректный запрос
-func TestMainHandlerWhenCorrectRequest(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?city=moscow&count=2", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), 2)
-}
-
-// Тест 2: Неподдерживаемый город
-func TestMainHandlerWhenWrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?city=paris&count=2", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
-}
-
-// Тест 3: Count больше общего количества
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := len(cafeList["moscow"])
-	req := httptest.NewRequest("GET", "/cafe?city=moscow&count="+strconv.Itoa(totalCount+1), nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	cafes := strings.Split(responseRecorder.Body.String(), ",")
-	assert.Len(t, cafes, totalCount)
 }
